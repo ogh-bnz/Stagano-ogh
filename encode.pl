d33833f0ce1966794595a91c51aa739ad8f51ee4
Team CLAY Hacker <90334239+ogh-bnz@users.noreply.github.com>
2023-10-31 01:08:25 +0600

    Add files via upload

---
+++ encode.pl
@@ -0,0 +1,8 @@
+
+Error in encoding.
+
+The input file is either of an unsupported type,
+or the payload is too large to be contained in the image.
+
+Use the "just find capacity..." option,
+which estimates the total size available for the payload.
