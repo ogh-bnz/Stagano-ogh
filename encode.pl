2ca26cbc3ccda297e7629d2a9a85669fbe427ac4
Team CLAY Hacker <90334239+ogh-bnz@users.noreply.github.com>
2023-10-31 02:12:15 +0600

    Delete encode.pl

--- encode.pl
+++
@@ -1,8 +0,0 @@
-
-Error in encoding.
-
-The input file is either of an unsupported type,
-or the payload is too large to be contained in the image.
-
-Use the "just find capacity..." option,
-which estimates the total size available for the payload.
